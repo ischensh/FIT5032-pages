59a47989de59beb1e61daf7bcc37db00022eee6e
Sche0408 <sche0408@student.monash.edu>
2025-10-23 20:31:02 +1100

    vite version

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "./FIT5032-pages",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
