c68ec61ced6dc9e8cf0f900ff337afba72dd6fcf
Sche0408 <sche0408@student.monash.edu>
2025-10-23 20:27:22 +1100

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About },
+  { path: "/", component: Home },
+  { path: "/about", component: About },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
